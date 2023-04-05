9eb53cd30622188f27f2144696f636698a3cd158
Ander-pirat <106609148+AndreiGubchenko@users.noreply.github.com>
2023-04-05 17:10:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -99,5 +99,5 @@ def contact():
     return render_template("contact.html")
 
 
-# if __name__ == '__main__':
-#     app.run(host="127.0.0.1", port=5000)
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000)
